75be7b9c0e02ae45e37e3b9c345fa5b2df3db02c
purple <athulkrishnamm51@gmail.com>
2025-11-15 21:00:17 +0530

    reconfigure backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,28 +8,7 @@ import bookingsRoutes from "./routes/bookings";
 const app = express();
 const PORT = 8080;
 
-const allowedOrigins = [
-    "https://nature-nest-resort.vercel.app/",
-    "nature-nest-resort-git-main-aks-projects-6b464eb3.vercel.app",
-    "nature-nest-resort-gxjzhar8b-aks-projects-6b464eb3.vercel.app"
-]
-
-const corsOptions = {
-    origin: function (origin: any, cb: any) {
-        if (!origin) return cb(null, true);
-        if (allowedOrigins.indexOf(origin) != -1) {
-            cb(null, true)
-        } else {
-            cb(new Error("Not allowed by CORS"))
-        }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "x-api-key"],
-    credentials: true,
-}
-
 app.use(express.json());
-app.use(cors({origin: "*"}))
 app.use(helmet())
 
 async function connectDB () {
